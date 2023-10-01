2d2b4bdf0316d9fca8bfd28259ba1a60d68192f5
TruongTanNghia <tannghiaxp@gmail.com>
2023-10-02 00:27:04 +0700

    #1 Setup rule project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+        Hello world!
+      </h1>
     </div>
   );
 }
