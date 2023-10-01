4e96c3aecb90e731baee6d10b17dfa99bac86587
TruongTanNghia <tannghiaxp@gmail.com>
2023-10-02 00:41:21 +0700

    #1 Update Setup rule project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
-        Hello world!
+        <img src={logo} className="w-10 h-10" alt="logo" />
+        Nghĩa 123
       </h1>
     </div>
   );
